a57c558aab80817bc0c9ead0b6cd9a084efc05b6
Ritesh Wagh <riteshwagh45@gmail.com>
2026-06-27 23:41:51 +0530

    abc

---
+++ src/collection/ArrayList/Employee1.java
@@ -0,0 +1,55 @@
+package collection.ArrayList;
+
+import java.util.ArrayList;
+
+public class Employee1 {
+    int id;
+    String name;
+    String address;
+
+   public Employee1(int id , String name , String address){
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
+
+   /*
+   @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+    */
+
+    public static void printReverseOrder(ArrayList<Employee1> list){
+        for(int i=list.size()-1; i>=0; i--){
+            Employee1 e = list.get(i);
+
+            System.out.println(e.id +" " +" "+ e.name +" "+ e.address);
+
+        }
+
+    }
+
+    public static void main(String[] args){
+       Employee1 E1 = new Employee1(1 , "ritesh" , "pune");
+       Employee1 E2 = new Employee1(2,"shubham","malegaon");
+       Employee1 E3 = new Employee1(3,"rahul","nsk");
+
+        ArrayList<Employee1> list = new ArrayList<>();
+        list.add(E1);
+        list.add(E2);
+        list.add(E3);
+
+        //System.out.println(list);
+
+        for(Employee1 e : list){
+            System.out.println(e.id+" ," + e.name +" , " + e.address);
+        }
+        printReverseOrder(list);
+
+    }
+}
---
+++ src/collection/ArrayList/Employee2.java
@@ -0,0 +1,70 @@
+/*
+Approach-2: (Create Employee objects by collecting details from arrays)
+Employee.java:
+• Create Employee class with instance variables id, name, salary
+• Define parameterized constructor to	initialize the object.
+ */
+
+
+package collection.ArrayList;
+
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+public class Employee2 {
+
+    public static class Employee{
+        private int id;
+        private String name;
+        private String address;
+
+        public Employee(int id , String name , String address){
+            this.id = id;
+            this.name = name;
+            this.address = address;
+        }
+    }
+
+    static void main(String[] args) {
+
+        int ids[] = {1,2,3,4,5};
+        String names[] = {"ritesh","shubham","rahul","gitesh","aryan"};
+        String addresses[] = {"pune","jalgaon","malegaon","malegaon","pune"};
+
+        ArrayList<Employee> list = new ArrayList<>();
+
+        for(int i=0; i<ids.length-1; i++){
+            Employee e = new Employee(ids[i], names[i], addresses[i]);
+            list.add(e);
+        }
+
+        //print data using for each loop
+        for(Employee e : list){
+            System.out.println(e.id +" "+" "+ e.name +" " +" " + e.address);
+
+        }
+
+        System.out.println("---------------------------------");
+        //print using ListIterator
+        ListIterator<Employee> itr = list.listIterator();
+
+        while(itr.hasNext()){
+            Employee e = itr.next();
+            System.out.println(" Id = "+e.id +" name = "+ e.name+" address = "+ e.address);
+
+        }
+
+        //printing the data in reverseorder using ListIterator
+        System.out.println("----------------------------------------");
+        ListIterator<Employee> itr2 = list.listIterator(list.size());
+        while(itr2.hasPrevious()){
+            Employee e = itr2.previous();
+            System.out.println(" Id = "+e.id +" name = "+ e.name+" address = "+ e.address);
+
+        }
+
+
+
+
+    }
+}
